56de3981d73d6bef873f91a598af2b33c5ca9282
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-06-08 16:17:28 +0200

    fix(utils): check for nullish env variables

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,11 +2,11 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const GITHUB_API_URL = process.env.GITHUB_API_URL
-const GITHUB_USER = process.env.GITHUB_USER
-const GITHUB_REPO = process.env.GITHUB_REPO
-const GITHUB_TARGET_REPO = process.env.GITHUB_TARGET_REPO
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+const GITHUB_API_URL = process.env.GITHUB_API_URL ?? ""
+const GITHUB_USER = process.env.GITHUB_USER ?? ""
+const GITHUB_REPO = process.env.GITHUB_REPO ?? ""
+const GITHUB_TARGET_REPO = process.env.GITHUB_TARGET_REPO ?? ""
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN ?? ""
 
 const config = {
   GITHUB_API_URL,
