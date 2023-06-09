b732cb0f9490cd729e57e6d20c743cd597272cf1
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-06-09 09:54:51 +0200

    fix(components): make `Footer` responsive
    Visualize in `flex-col` if screen is smaller than 768px otherwise
    visualize in `flex-row`

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ export default async function Footer() {
   const version = await getVersion()
 
   return (
-    <footer className="flex p-2 bg-zinc-900 border-t border-zinc-800 text-xs text-white">
-      <span className="flex-1 text-center">Version {version.tag_name}</span>
-      <span className="flex-1 text-center">Copyright © 2023 - Francesco Aliprandi</span>
-      <span className="flex-1 text-center">aliprandifrancescopp@gmail.com</span>
+    <footer className="flex flex-col gap-2 p-2 bg-zinc-900 border-t border-zinc-800 text-xs text-white md:flex-row">
+      <span className="flex-1 text-center underline">Version {version.tag_name}</span>
+      <span className="flex-1 text-center underline">Copyright © 2023 - Francesco Aliprandi</span>
+      <span className="flex-1 text-center underline">aliprandifrancescopp@gmail.com</span>
     </footer>
   )
 }
