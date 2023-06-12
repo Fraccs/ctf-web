92adb0196e089ee9e16bd1fd1d87c2080f8a9358
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-06-12 17:40:27 +0200

    feat(app): create `error` & `loading` pages

---
+++ src/app/error.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+interface ErrorProps {
+  error: Error,
+  reset: () => void
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  return (
+    <main className="h-full flex items-center justify-center bg-zinc-950 text-white overflow-y-scroll">
+      <div className="flex flex-col gap-8 items-center">
+        <div className="font-bold text-4xl">Oops.. {error.message}.</div>
+        <button onClick={reset} type="button" className="px-16 py-2 rounded-lg bg-gradient-to-br from-red-600 via-rose-600 to-red-500 font-bold text-center text-white shadow-2xl drop-shadow-2xl shadow-rose-600 hover:opacity-90">
+          Retry
+        </button>
+      </div>
+    </main>
+  )
+}
