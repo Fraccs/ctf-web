6c6a113098f1a2ac3b2b90b55df530208af1f663
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-06-29 14:14:27 +0200

    fix(interfaces): create `GithubGitTree` & `GithubGitTreeItem` interfaces

--- src/interfaces/github.ts
+++ src/interfaces/github.ts
@@ -1,31 +1,15 @@
-export interface GitHubRepoContent {
-  name: string;
-  path: string;
-  sha: string;
-  size: number;
-  url: string;
-  html_url: string;
-  git_url: string;
-  download_url: string | null;
-  type: string;
-  content?: string;
-  encoding?: string;
-  _links?: {
-    self: string;
-    git: string;
-    html: string;
-  };
+export interface GithubGitTree {
+  sha: string
+  url: string
+  truncated: boolean
+  tree: GithubGitTreeItem[]
 }
 
-export interface GithubVersion {
-  url: string,
-  html_url: string,
-  id: number,
-  tag_name: string,
-  name: string,
-  draft: false,
-  prerelease: false,
-  created_at: Date,
-  published_at: Date,
-  assets?: [],
+export interface GithubGitTreeItem {
+  path: string
+  mode: string
+  type: "blob" | "tree"
+  sha: string
+  size: number
+  url: string
 }
