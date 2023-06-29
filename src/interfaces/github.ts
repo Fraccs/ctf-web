92d74b8956ce5f60ac8fbc4ed4dc7fbe1e9ab178
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-06-29 14:48:43 +0200

    revert(interfaces): undo `GithubRepoContent` & `GithubVersion` deletion

--- src/interfaces/github.ts
+++ src/interfaces/github.ts
@@ -13,3 +13,35 @@ export interface GithubGitTreeItem {
   size: number
   url: string
 }
+
+export interface GitHubRepoContent {
+  name: string
+  path: string
+  sha: string
+  size: number
+  url: string
+  html_url: string
+  git_url: string
+  download_url: string | null
+  type: string
+  content?: string
+  encoding?: string
+  _links?: {
+    self: string
+    git: string
+    html: string
+  }
+}
+
+export interface GithubVersion {
+  url: string
+  html_url: string
+  id: number
+  tag_name: string
+  name: string
+  draft: false
+  prerelease: false
+  created_at: Date
+  published_at: Date
+  assets?: []
+}
