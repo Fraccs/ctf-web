4e82dc5e3be808f7c2408d7754801f9b96f8c23a
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-06-29 14:20:36 +0200

    feat(*): create and implement `VisualizeTree` component
    The component currently displays the contents of a tree (directory)
    identified by its sha, the internal directories of a challenge shouldn't
    be visualized but the component currently does and this behaviour should
    be fixed

--- src/app/competitions/page.tsx
+++ src/app/competitions/page.tsx
@@ -1,32 +1,7 @@
-import Link from "next/link"
-import githubService from "@/services/github"
-import config from "@/utils/config"
-import { GitHubRepoContent } from "@/interfaces/github"
-
-const getCompetitions = async () => {
-  const response = await githubService.apiRequest<GitHubRepoContent[]>({
-    url: `/repos/${config.GITHUB_USER}/${config.GITHUB_TARGET_REPO}/contents`
-  })
-
-  return response.data
-}
+import VisualizeTree from "@/components/VisualizeTree"
 
 export default async function Competitions() {
-  const competitions = await getCompetitions()
-
   return (
-    <main className="h-full bg-zinc-950 text-white overflow-y-scroll">
-      <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
-        {competitions?.filter(c => c.type === "dir").map((competition, i) => {
-          return (
-            <Link key={i} href={`/competitions/${competition.name}`}>
-              <article className="h-full flex flex-col rounded-lg bg-zinc-900 p-4 border border-zinc-800 shadow-lg">
-                <span className="mt-auto rounded-lg px-4 py-2 border border-zinc-800 font-bold text-sm text-center">{competition.name}</span>
-              </article>
-            </Link>
-          )
-        })}
-      </div>
-    </main>
+    <VisualizeTree/>
   )
 }
