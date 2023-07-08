e304dc2bf3d3805f18fa6bf7c96215b60f430baf
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-07-08 14:57:02 +0200

    fix(components): detect max tree depth and stop further navigation
    Detect challenge directories and stop further navigation to challenge
    subfolders

--- src/components/VisualizeTree.tsx
+++ src/components/VisualizeTree.tsx
@@ -20,6 +20,52 @@ const getRepoTree = async (sha?: string, recursive?: boolean) => {
 export default async function VisualizeTree({ sha }: VisualizeTreeProps) {
   const tree = await getRepoTree(sha)
 
+  const maxDepthReached = tree?.some(node => (
+    node.type === "blob" &&
+    node.path !== "writeups-template.md" &&
+    node.path !== "README.md" &&
+    node.path !== ".gitignore"
+  ))
+
+  if(maxDepthReached) {
+    return (
+      <div className="h-full w-full bg-zinc-950 text-white overflow-y-scroll">
+        <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
+          {tree?.map(async item => {
+            if(item.type === "tree") {
+              return <></>
+            }
+
+            if(item.path === "flag.txt") {
+              return (
+                <article key={item.sha} className="h-full flex flex-col rounded-lg bg-zinc-900 p-4 border border-zinc-800 shadow-lg">
+                  <div className="flex items-center justify-center rounded-lg px-4 py-2 border border-zinc-800">
+                    <AiFillFlag className="text-xl text-red-600"/>
+                    <span className="mx-auto font-bold text-sm text-center">{item.path}</span>
+                  </div>
+                </article>
+              )
+            }
+
+            if(item.path === "writeup.md") {
+              const response = await githubService.apiRequest<GitHubRepoContent>({
+                url: `/repos/${config.GITHUB_USER}/${config.GITHUB_TARGET_REPO}/git/blobs/${item.sha}`
+              })
+
+              const markdown = atob(response.data.content ?? "")
+
+              return (
+                <div key={item.sha}>
+                  <Markdown markdown={markdown}/>
+                </div>
+              )
+            }
+          })}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="h-full w-full bg-zinc-950 text-white overflow-y-scroll">
       <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
@@ -34,31 +80,6 @@ export default async function VisualizeTree({ sha }: VisualizeTreeProps) {
                   </div>
                 </article>
               </Link>
-            )
-          }
-
-          if(item.path === "flag.txt") {
-            return (
-              <article key={item.sha} className="h-full flex flex-col rounded-lg bg-zinc-900 p-4 border border-zinc-800 shadow-lg">
-                <div className="flex items-center justify-center rounded-lg px-4 py-2 border border-zinc-800">
-                  <AiFillFlag className="text-xl text-red-600"/>
-                  <span className="mx-auto font-bold text-sm text-center">{item.path}</span>
-                </div>
-              </article>
-            )
-          }
-
-          if(item.path === "writeup.md") {
-            const response = await githubService.apiRequest<GitHubRepoContent>({
-              url: `/repos/${config.GITHUB_USER}/${config.GITHUB_TARGET_REPO}/git/blobs/${item.sha}`
-            })
-
-            const markdown = atob(response.data.content ?? "")
-
-            return (
-              <div key={item.sha}>
-                <Markdown markdown={markdown}/>
-              </div>
             )
           }
         })}
