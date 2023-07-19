baf2c33205309795f1895e324f1e6255531bcc06
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-07-19 18:50:14 +0200

    patch(components): implement code highlighting in `Markdown`

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -2,6 +2,8 @@
 
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
+import SyntaxHighlighter from "react-syntax-highlighter"
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
 import { twMerge } from "tailwind-merge"
 
 type MarkdownProps = {
@@ -11,7 +13,27 @@ type MarkdownProps = {
 
 export default function Markdown({ markdown, className }: MarkdownProps) {
   return (
-    <ReactMarkdown remarkPlugins={[remarkGfm]} className={twMerge("prose prose-invert font-mono", className)}>
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "")
+
+          return !inline && match ?
+            <SyntaxHighlighter
+              {...props}
+              style={vscDarkPlus}
+              language={match[1]}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter> :
+            <code {...props} className={className}>
+              {children}
+            </code>
+        }
+      }}
+      className={twMerge("prose prose-invert font-mono", className)}
+    >
       {markdown}
     </ReactMarkdown>
   )
