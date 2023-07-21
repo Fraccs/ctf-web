3fc36cfdbde5f33d08dd2f96bc20858191e354fd
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-07-21 17:37:57 +0200

    patch(app): remove `/competitions` route
    Navigate directly to `/competitions/<root_sha>` when the competitions
    button is clicked in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link"
+import { getGithubGitMainSha } from "@/utils/github"
 
-export default function Page() {
+export default async function Page() {
+  const sha = await getGithubGitMainSha()
+
   return (
     <main className="h-full flex flex-col items-center justify-center gap-16 bg-zinc-950">
       <span className="p-2 font-bold text-5xl text-center text-transparent italic bg-clip-text bg-gradient-to-r from-zinc-950 via-white to-zinc-950 animate-gradient-x">Collection of <span className="text-rose-600">CTF</span> solves and writeups</span>
@@ -8,7 +11,7 @@ export default function Page() {
         <Link href="/readme" className="px-4 py-2 rounded-lg bg-gradient-to-br bg-white font-bold text-center shadow-2xl drop-shadow-2xl shadow-white hover:opacity-90">
           README.md
         </Link>
-        <Link href="/competitions" className="px-4 py-2 rounded-lg bg-gradient-to-br from-red-600 via-rose-600 to-red-500 font-bold text-center text-white shadow-2xl drop-shadow-2xl shadow-rose-600 hover:opacity-90">
+        <Link href={`/competitions/${sha}`} className="px-4 py-2 rounded-lg bg-gradient-to-br from-red-600 via-rose-600 to-red-500 font-bold text-center text-white shadow-2xl drop-shadow-2xl shadow-rose-600 hover:opacity-90">
           Competitions
         </Link>
       </div>
