25dcd63b82197bc05f9784d73c3170850350f361
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-08-05 16:52:35 +0200

    patch(components): add login apiRequest to `LoginForm`

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,16 +1,58 @@
 "use client"
 
-import { FormEvent, useState } from "react"
-import { AiOutlineEye, AiOutlineEyeInvisible, AiOutlineKey, AiOutlineLogin, AiOutlineUser } from "react-icons/ai"
+import { useRouter } from "next/navigation"
+import { FormEvent, useEffect, useState } from "react"
+import { AiOutlineCloseCircle, AiOutlineEye, AiOutlineEyeInvisible, AiOutlineKey, AiOutlineLogin, AiOutlineUser } from "react-icons/ai"
+import apiService from "@/services/api"
 
 export default function LoginForm() {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [isPasswordShown, setIsPasswordShown] = useState(false)
 
+  const [isError, setIsError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { push } = useRouter()
+
   const onSubmit = async (e: FormEvent) => {
     e.preventDefault()
+
+    if(!username || !password) {
+      return
+    }
+
+    try {
+      setIsLoading(true)
+
+      await apiService.apiRequest({
+        url: "/auth",
+        method: "POST",
+        data: {
+          username,
+          password
+        }
+      })
+
+      push("/")
+    }
+    catch(e) {
+      setIsError(true)
+    }
+    finally {
+      setIsLoading(false)
+      setUsername("")
+      setPassword("")
+    }
   }
+
+  useEffect(() => {
+    if(isError) {
+      setTimeout(() => {
+        setIsError(false)
+      }, 2000)
+    }
+  }, [isError])
 
   return (
     <form onSubmit={onSubmit} className="w-96 flex flex-col items-center gap-4 text-white">
@@ -41,10 +83,17 @@ export default function LoginForm() {
             <AiOutlineEyeInvisible className="hover:opacity-90"/>
           </button>}
       </div>
-      <button type="submit" className="w-full px-4 py-2 rounded-md border border-zinc-800 bg-gradient-to-r from-zinc-900 to-zinc-800 hover:opacity-90">
+      <button type="submit" className={`w-full px-4 py-2 rounded-md border border-zinc-800 bg-gradient-to-r ${isLoading || isError ? "from-zinc-800 via-rose-600 to-zinc-800 animate-gradient-x animate-pulse" : "from-zinc-900 to-zinc-800"} hover:opacity-90`}>
         <div className="flex items-center justify-center gap-2">
-          <AiOutlineLogin className="text-xl"/>
-          <span className="font-bold">Login</span>
+          {isError ?
+            <>
+              <AiOutlineCloseCircle className="text-xl"/>
+              <span className="font-bold">Error</span>
+            </> :
+            <>
+              <AiOutlineLogin className="text-xl"/>
+              <span className="font-bold">Login</span>
+            </>}
         </div>
       </button>
     </form>
