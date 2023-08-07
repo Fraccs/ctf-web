b2a242e5dffa0c1f5d296f23527cace68c18a157
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-08-07 13:08:32 +0200

    fix(app): redirect to "/" from "/login" if auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,14 @@
 import LoginForm from "@/components/LoginForm"
+import serverUseAuth from "@/hooks/serverUseAuth"
+import { redirect } from "next/navigation"
 
 export default function Page() {
+  const auth = serverUseAuth()
+
+  if(auth) {
+    redirect("/")
+  }
+
   return (
     <main className="h-full w-full flex flex-col items-center justify-center bg-zinc-950">
       <div className="flex flex-col items-center justify-center gap-8">
