7ee69edeff30408701c993fb244b7fb25e4b8325
Aliprandi Francesco <aliprandifrancescopp@gmail.com>
2023-08-07 12:52:43 +0200

    feat(components): implement authentication
    Add flag visualization in `Flag` (if user has admin permissions)

--- src/components/Flag.tsx
+++ src/components/Flag.tsx
@@ -1,16 +1,28 @@
 "use client"
 
-import { AiFillFlag } from "react-icons/ai"
+import { useState } from "react"
+import { AiFillFlag, AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
 import { GitHubRepoContent } from "@/types/github"
 
-type FlagProps = Pick<GitHubRepoContent, "sha" | "path">
+type FlagProps = Pick<GitHubRepoContent, "sha" | "path" | "content">
 
-export default function Flag({ path, sha }: FlagProps) {
+export default function Flag({ path, sha, content }: FlagProps) {
+  const [isVisible, setIsVisible] = useState(false)
+
   return (
     <article key={sha} className="h-min flex flex-col rounded-lg bg-zinc-900 p-4 border border-zinc-800 shadow-lg">
       <div className="flex items-center justify-center rounded-lg px-4 py-2 border border-zinc-800">
         <AiFillFlag className="text-xl text-red-600"/>
-        <span className="mx-auto font-mono text-sm text-center">{path}</span>
+        <span className="mx-auto font-mono text-sm text-center">
+          {isVisible ? (content ?? "Only admins can view flags!") : path}
+        </span>
+        {isVisible ?
+          <button type="button" onClick={() => setIsVisible(false)}>
+            <AiOutlineEye className="text-xl hover:opacity-90"/>
+          </button> :
+          <button type="button" onClick={() => setIsVisible(true)}>
+            <AiOutlineEyeInvisible className="text-xl hover:opacity-90"/>
+          </button>}
       </div>
     </article>
   )
